7457deeed04ea15439a80ed926560a2676b1fad6
Derek-arima <darima@byui.edu>
2024-06-08 00:53:37 -0600

    created and populated README

--- Grocerylist.java
+++ Grocerylist.java
@@ -8,7 +8,11 @@ public class Grocerylist {
         List<String> items = new ArrayList<String>();
         String userChoice = ""; 
 
+
+// This is a while loop that will keep running until the user chooses to exit the program
         while (userChoice !="4"){
+
+            // creating a scanner object that will be used to read the users input
             Scanner userInput = new Scanner(System.in);
             System.out.println("Welcome to your grocery list! what would you like to do?");
             System.out.println("1. Add a new item");
@@ -16,10 +20,11 @@ public class Grocerylist {
             System.out.println("3. Delete an item");
             System.out.println("4. Exit");
 
+            // reading the users input
             userChoice = userInput.next();
             System.out.println("");
             
-            
+            // This is a series of if statements that will run based on the users input
             if (userChoice.equals("1")) {
                 createItem(items);
             }
@@ -49,6 +54,7 @@ public class Grocerylist {
         }
     }
 
+    // This is a method that will create a new item and add it to the list
     static void createItem(List<String> items) {
         Scanner name = new Scanner(System.in);
         System.out.println("Enter the name of the item you would like to add: ");
@@ -59,6 +65,8 @@ public class Grocerylist {
 
     }
 
+
+    // This is a method that will view all the items in the list
     static void viewItems(List<String> items) {
         Iterator<String> iterator = items.iterator();
         while (iterator.hasNext()) {
@@ -66,6 +74,8 @@ public class Grocerylist {
         }
     }
 
+
+    // This is a method that will delete an item from the list
     static void deleteItem(List<String> items) {
         Scanner name = new Scanner(System.in);
         System.out.println("Enter the name of the item you would like to delete: ");
---
+++ JavaREADME.md
@@ -0,0 +1,32 @@
+ # Overview
+
+
+
+{This program will help a user keep track of their grocery list. The user will be able to add items to the list, remove items from the list, and print the list.}
+
+{I wrote this program to help me demonstrate my understanding of the basics of Java. I wanted to create a simple program that would help me to remember what I needed to get at the store.}
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](https://youtu.be/e5VcMR7qzuc)
+
+# Development Environment
+
+{Visusal Sudio Code was used to write this program. I also needed to download the Java Development Kit (JDK) to run the program.}
+
+{The programming language used was Java.}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Web Site Name](https://www.w3schools.com/java/default.asp)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- I want to make the interface a little nicer and easier to read
+- I want to add a feature that will allow the user to check off items on the list
+- I want to add a feature that will allow the user to sort the list alphabetically
+
